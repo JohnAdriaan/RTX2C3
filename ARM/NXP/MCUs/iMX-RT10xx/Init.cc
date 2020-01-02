8fb3b6e1a48d1c146f1206d25a0051d09699abcf
John Adriaan <John.Adriaan@BigPond.com>
2020-01-03 01:00:01 +1100

    Disable RTWatchdog

--- ARM/NXP/MCUs/iMX-RT10xx/Init.cc
+++ ARM/NXP/MCUs/iMX-RT10xx/Init.cc
@@ -5,10 +5,38 @@
 
 #include <ARM/NXP/MCUs/iMX-RT1011.hh>
 
+#include <ARM/NXP/Timers/WDogs/RTWDog.hh>
+
+using namespace ARM::NXP::Timers;
+using namespace ARM::NXP::Timers::WDogs;
+
 #ifdef NXP_iMXRT10xx
 
+static void DisableWatchdogs() {
+	// Disable RTWDOG
+	WDogs::FieldCS cs = (FieldCS &)rtwDog.CS; // Copy for multiple accesses
+
+	// Allowed to unlock RTWDOG?
+    if (cs.UPDATE) {
+    	// Unlock according to current `CMD32EN` requirements
+		if (cs.CMD32EN) {
+		   rtwDog.CNT = CNTs::Unlock;        // Full 32-bit unlock required
+		} // if
+		else {
+		   cs.CMD32EN = true;                // Enable for next time
+		   rtwDog.CNT.CNT16 = CNTs::Unlock1; // 16-bit Low followed
+		   rtwDog.CNT.CNT16 = CNTs::Unlock2; // by 16-bit high
+		} // else
+		rtwDog.TOVAL.TOVAL16 = ~0; // Longest timeout
+		cs.EN = false;             // Disable!
+		rtwDog.CS = cs;            // Write back
+    } // if
+} // DisableWatchdogs()
+
 void ARM::NXP::Init() {
-	/// Initialise OCRAM?
+	// Initialise OCRAM?
+
+	DisableWatchdogs();
 } // ARM::NXP::Init()
 
 #endif // NXP_iMXRT10xx
